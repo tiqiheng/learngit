445efc4689493fc67c1c769878cf810a5a02bc63
tiqh <Tiqh@TIQH.ha.cnc.intra>
2018-04-10 07:54:54 +0800

    add array_ex and modify code_ch04

--- ch04/code_ch04.py
+++ ch04/code_ch04.py
@@ -118,4 +118,119 @@ points = np.arange(-5,5,0.01)
 
 xs, ys = np.meshgrid(points, points)
 
+import matplotlib.pyplot as plt
 
+z = np.sqrt(xs**2 + ys**2)
+
+z
+
+plt.imshow(z, cmap=plt.cm.gray);
+plt.colorbar()
+
+plt.title("Image plot of $\sqrt{x^2+y^2}$ for a grid of values")
+
+#####$$将条件逻辑表述为数组运算$$#####
+
+xarr = np.array([1.1,1.2,1.3,1.4,1.5])
+
+yarr = np.array([2.1,2.2,2.3,2.4,2.5])
+
+cond = np.array([True, False, True, True, False])
+
+result = [(x if c else y) for x, y, c in zip(xarr, yarr, cond)]
+
+result = np.where(cond, xarr, yarr)
+
+
+result = np.where(xarr >= 1.3, 2, 1)
+
+
+arr = randn(4)
+
+np.where(arr > 0, 2, -2)
+
+np.where(arr>0, 2, arr)
+
+
+np.where(cond1 & cond2, 0, np.where(cond1, 1, np.where(cond2, 2,3)))
+
+result = []
+for i in range(n):
+	if cond1[i] and cond2[i]:
+		result.append(0)
+	elif cond1[i]:
+		result.append(1)
+	elif cond2[i]:
+		result.append(2)
+	else:
+		result.append(3)
+		
+
+#####$$数学和统计方法$$#####
+arr = np.random.randn(5,4)    #正态分布的数据
+arr.mean()
+np.random.randn(5,4)
+
+np.mean(arr)
+
+arr.sum()
+
+
+arr.mean(axis=1)
+arr.sum(0)
+
+arr = np.arange(0,9).reshape(3,3)
+
+arr = randn(100)
+
+(arr > 0).sum()
+
+bools = np.array([True, False, True, True, False])
+
+bools.any() #用于检查数组中是否有一个或多个True
+
+bools.all() #用于检查数组中所有值是否都是True
+
+arr = randn(8)
+
+arr.sort()
+
+arr = randn(5,3)
+
+arr.sort(1)
+
+large_arr = randn(1000)
+
+large_arr.sort()
+
+large_arr[int(0.05*len(large_arr))]
+
+step = np.arange(0,1,0.05)
+
+large_arr[int(1000*step)]
+
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+
+np.unique(names)
+
+
+ints = np.array([3,3,3,2,2,1,1,4,4])
+np.unique(ints)
+
+
+values = np.array([6,0,0,3,2,5,6])
+np.in1d(values, [2,3,6])
+
+
+#####$$将数组以二进制格式保存磁盘$$#####
+
+arr = np.arange(10)
+
+np.save('some_array', arr)
+
+np.load('some_array.npy')
+
+np.savez('array_archive.npz', a=arr, b=arr)
+
+arch = np.load('array_archive.npz')
+arch['b']
