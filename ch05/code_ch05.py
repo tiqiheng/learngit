39322798895533acb49765bcc959de0f9e880a47
Tiqh <tiqiheng.001@163.com>
2018-04-21 23:44:02 +0800

    添加DataFrame与Series的运算及排序和排名

--- ch05/code_ch05.py
+++ ch05/code_ch05.py
@@ -153,3 +153,138 @@ s2 = Series([-2.1, 3.6, -1.5, 4, 3.1], index=['a', 'c', 'e', 'f', 'g'])
 df1 = DataFrame(np.arange(9.).reshape((3,3)), columns=list('bcd'), index=['Ohio', 'Texas', 'Colorado'])
 
 df2 = DataFrame(np.arange(12.).reshape((4,3)), columns=list('bde'), index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+
+#####$$在算数方法中填充值$$#####
+df1 = DataFrame(np.arange(12.).reshape((3,4)), columns=list('abcd'))
+
+df2 = DataFrame(np.arange(20.).reshape((4,5)), columns=list('abcde'))
+
+df1 + df2
+
+df1.reindex(columns=df2.columns, fill_value=0)
+
+#####$$DataFrame和Series之间的运算$$#####
+arr = np.arange(12.).reshape((3,4))
+arr - arr[0]
+
+frame = DataFrame(np.arange(12.).reshape((4,3)), columns=list('bde'), index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+
+series = frame.iloc[0]
+
+frame
+
+data = DataFrame(np.arange(16).reshape((4,4)), index = ['Ohio','Colorado','Utah','New York'], columns = ['one', 'two', 'three', 'four'])
+
+frame - series
+series2 = Series(range(3), index=['b', 'e', 'f'])
+frame + series2
+
+series3 = frame['d']
+
+
+
+# fig = plt.figure()
+
+# for i in range(0,4):
+	# fig.add_subplot(2,2,i + 1)
+	# plt.plot(data.iloc[:,i])
+	# plt.title(data.index[i])
+# plt.show()
+	
+# output = open('data.txt', 'w+')
+# str = ""
+
+
+#####$$函数和映射$$#####
+frame = DataFrame(np.random.randn(4,3), columns=list('bde'), index=['Utah', 'Ohio', 'Texas', 'Oregon']
+
+# result:
+               # b         d         e
+# Utah   -0.602319  1.261217  0.927547
+# Ohio   -0.162878 -0.413711 -0.341497
+# Texas  -0.736304 -0.956781  0.167785
+# Oregon  1.740673  0.427462  0.359928
+frame
+
+np.abs(frame)
+# result:
+               # b         d         e
+# Utah    0.602319  1.261217  0.927547
+# Ohio    0.162878  0.413711  0.341497
+# Texas   0.736304  0.956781  0.167785
+# Oregon  1.740673  0.427462  0.359928
+
+
+
+In [18]: f=lambda x: x.max() - x.min()
+
+In [19]: frame.apply(f)
+Out[19]:
+b    2.476977
+d    2.217998
+e    1.269044
+dtype: float64
+
+In [20]: frame.apply(f, axis = 1)
+Out[20]:
+Utah      1.863536
+Ohio      0.250832
+Texas     1.124566
+Oregon    1.380745
+dtype: float64
+
+
+In [22]: def f(x):
+    ...:     return Series([x.min(), x.max()], index=['min', 'max'])
+    ...:
+
+In [23]: frame.apply(f,axis=1)
+Out[23]:
+             min       max
+Utah   -0.602319  1.261217
+Ohio   -0.413711 -0.162878
+Texas  -0.956781  0.167785
+Oregon  0.359928  1.740673
+
+In [24]: frame.apply(f)
+Out[24]:
+            b         d         e
+min -0.736304 -0.956781 -0.341497
+max  1.740673  1.261217  0.927547
+
+# applymap用于DataFrame， map用于Series
+
+In [26]: format = lambda x: '%.2f'%x
+
+In [27]: frame.applymap(format)
+Out[27]:
+            b      d      e
+Utah    -0.60   1.26   0.93
+Ohio    -0.16  -0.41  -0.34
+Texas   -0.74  -0.96   0.17
+Oregon   1.74   0.43   0.36
+
+In [28]: frame['e'].map(format)
+Out[28]:
+Utah       0.93
+Ohio      -0.34
+Texas      0.17
+Oregon     0.36
+Name: e, dtype: object
+
+
+#####$$排序和排名$$#####
+
+
+In [30]: obj = Series(range(4), index=['d', 'a', 'b', 'c'])
+
+In [31]: obj.sort_index()
+Out[31]:
+a    1
+b    2
+c    3
+d    0
+dtype: int64
+
+frame = DataFrame(np.arange(8).reshape((2,4)), index=['three', 'one'], columns=['d', 'a', 'b', 'c'])
+
